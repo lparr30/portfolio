829a9b4d19305de749ddd3bf640d03c52bcc24da
Liza Parr <lizajparr@gmail.com>
2023-06-07 16:34:43 -0400

    deployed website

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,21 @@
+import night from '../assets/night-sky.jpeg';
+import sky from '../assets/sky.jpeg';
+import beach from '../assets/beach.jpeg';
+
 function Projects(){return(
     <div className='margin'>
         <h2>Projects</h2>
-        <p>Project 1</p>
-        <p>Project 2</p>
-        <p>Project 3</p>
+        <div className='projects'>
+            <div>
+                <a href="http://www.google.com" target="_blank"><img className='sky' src={sky} alt="sky" /></a>
+            </div>
+            <div>
+                <a href="http://www.google.com" target="_blank"><img className='night' src={night} alt="night" /></a>
+            </div>
+            <div>
+                <a href="http://www.google.com" target="_blank"><img className='beach' src={beach} alt="beach" /></a>
+            </div>
+        </div>
     </div>
 )}
 
