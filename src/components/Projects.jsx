4b4c416fe40b65a871fd945c97041f9900bc7438
Liza Parr <lizajparr@gmail.com>
2023-06-13 11:12:09 -0400

    new branch for hover effect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,15 @@
 import night from '../assets/night-sky.jpeg';
 import sky from '../assets/sky.jpeg';
 import beach from '../assets/beach.jpeg';
-// import { useState } from 'react';
+import { useState } from 'react';
 
-function hover(event) {
-    const target = event.target.parentElement.parentElement.children[1]
-    console.log(event.target);
-    if (target.classList.contains('hide')){
-        target.classList.remove('hide')
-        target.classList.add('show')
-    } else {
-        target.classList.remove('show')
-        target.classList.add('hide')
-    }
-}
+function Overlay(props) {return(
+    <>
+    {props.isOpen && (
+        <div className='overlay'>{props.children}</div>
+    )}
+    </>
+)}
 
 function Projects(){return(
     <div className='margin'>
@@ -21,11 +17,6 @@ function Projects(){return(
         <div className='projects'>
             <div onMouseOver={hover}>
                 <a href="http://www.google.com" target="_blank"><img className='sky' src={sky} alt="sky" /></a>
-                <div id='test' className='hide overlay'>
-                    <p>this is a test description</p>
-                    <button>Repo</button>
-                    <button>Deployed App</button>
-                </div>
             </div>
             <div onMouseOver={hover}>
                 <a href="http://www.google.com" target="_blank"><img className='night' src={night} alt="night" /></a>
