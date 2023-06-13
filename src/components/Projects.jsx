901361bed8ac8bde9dc1a120a27e5da8062b81f9
Liza Parr <lizajparr@gmail.com>
2023-06-13 12:27:40 -0400

    beginning of overlay code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,29 +3,59 @@ import sky from '../assets/sky.jpeg';
 import beach from '../assets/beach.jpeg';
 import { useState } from 'react';
 
+function repo() {
+    console.log('repo');
+}
+
+function deployed() {
+    console.log('deployed');
+}
+
+function closeOverlay() {
+    console.log('x');
+}
+
 function Overlay(props) {return(
     <>
-    {props.isOpen && (
-        <div className='overlay'>{props.children}</div>
+    {props.isClicked && (
+        <div className='overlay'>
+            <p>{props.description}</p>
+            <button onClick={repo}>Repository</button>
+            <button onClick={deployed}>Deployed App</button>
+            <button onClick={closeOverlay}>X</button>
+        </div>
     )}
     </>
 )}
 
-function Projects(){return(
-    <div className='margin'>
-        <h2 id='projectsSection'>Projects</h2>
-        <div className='projects'>
-            <div onMouseOver={hover}>
-                <a href="http://www.google.com" target="_blank"><img className='sky' src={sky} alt="sky" /></a>
-            </div>
-            <div onMouseOver={hover}>
-                <a href="http://www.google.com" target="_blank"><img className='night' src={night} alt="night" /></a>
-            </div>
-            <div onMouseOver={hover}>
-                <a href="http://www.google.com" target="_blank"><img className='beach' src={beach} alt="beach" /></a>
+function Projects(){
+    
+    const [isClicked, setIsClicked] = useState(false);
+
+    function toggleOverlay() {
+        setIsClicked(!isClicked);
+        console.log("i got clicked")
+    }
+    
+    return(
+        <div className='margin'>
+            <h2 id='projectsSection'>Projects</h2>
+            <div className='projects'>
+                <div>
+                    <img className='sky' src={sky} alt="sky" onClick={toggleOverlay}/>
+                    <Overlay description='Test description'/>
+                    {/* <a href="http://www.google.com" target="_blank"></a> */}
+                </div>
+                <div>
+                    <img className='night' src={night} alt="night" onClick={toggleOverlay}/>
+                    {/* <a href="http://www.google.com" target="_blank"></a> */}
+                </div>
+                <div>
+                    <img className='beach' src={beach} alt="beach" onClick={toggleOverlay}/>
+                    {/* <a href="http://www.google.com" target="_blank"></a> */}
+                </div>
             </div>
         </div>
-    </div>
 )}
 
 export default Projects;
