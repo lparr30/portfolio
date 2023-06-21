bc3c7040f95a580ec8322bee2f35b117c63341be
Liza Parr <lizajparr@gmail.com>
2023-06-21 09:50:29 -0400

    projects cards smaller on larger screens

--- src/components/projectSection/ProjectCard.jsx
+++ src/components/projectSection/ProjectCard.jsx
@@ -1,48 +1,30 @@
 import Overlay from './Overlay';
 import { useState } from 'react';
-import useWindowDimensions from '../../hooks/useWindowDimensions';
-// import WindowWidth from './WindowWidth';
 
 function ProjectCard({ id, fileName, alt, title, description  }) {
     const [isClicked, setIsClicked] = useState(false);
     const [isHovered, setIsHovered] = useState(false);
 
-    const { width } = useWindowDimensions();
-    const isMobileDevice = width <= 768;
-    
-    const handleClick = (event) => {
-        if (isMobileDevice){
- 
-            if (isClicked && event.target.classList.contains("closeOverlay")) {
-                    setIsClicked(false);
-            } else {
-                setIsClicked(true);
-            }
-        }
+    const handleClick = () => {
+        setIsClicked(!isClicked);
     };
-    
+
     const handleMouseEnter = () => {
-        if (!isMobileDevice) {
-            setIsHovered(true);
-        }
+        setIsHovered(true);
     };
     
     const handleMouseLeave = () => {
-        if (!isMobileDevice) {
-            setIsHovered(false);
-        }
-    };
-        
-    // let screenSize = window.innerWidth < 769 ? handleClick() : handleMouseEnter();
+        setIsHovered(false);
+    }
 
     return(
         <div 
             className="project-card"
-            onClick={(event) => handleClick(event)}
+            onClick={handleClick}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-        >
-                <Overlay isClicked={isClicked} isHovered={isHovered} setIsClicked={setIsClicked} isMobileDevice={isMobileDevice} title={title} description={description}/>
+            >
+                <Overlay isClicked={isClicked || isHovered} setIsClicked={setIsClicked} title={title} description={description}/>
                 {/* <img id={id} src={fileName} alt={alt} onClick={() => setIsClicked(!isClicked)}/> */}
                 <img id={id} src={fileName} alt={alt}/>
         </div>
