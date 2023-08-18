c0ce3f7f4576d1b25afb8d8a7c07d0e57d343d8e
Liza Parr <lizajparr@gmail.com>
2023-08-18 12:25:35 -0400

    adds intodoitive to projects

--- src/components/projectSection/CardInfo.js
+++ src/components/projectSection/CardInfo.js
@@ -7,25 +7,25 @@ const cardInfo = [
         title: 'Happiest Hour',
         description: 'is a collaborative React app on which I spearheaded frontend development while my colleague tackled backend inteicacies. This project provided many challenges both anticipated and unforeseen. Ultimately, our efforts culminated in a sophisticated yet user-friendly application, empowering individuals to effortlessly explore and discover top-rated happy hour offerings within a specified zip code, thereby enhancing their social experiences.',
         image: happiestHour,
-        alt: 'cloudy sky',
+        alt: 'Landing page of Happiest Hour',
         repoLink: 'https://github.com/ColdWeatherBoyy/happiest-hour',
         deployLink: 'https://happiest-hour-cb7f5c7640b4.herokuapp.com/'
+    },
+    {
+        picName: 'night',
+        title: 'Intodoitive',
+        description: 'is the most intuitive to-do list app on the market. Easy to use and thoughtfully structured, Intodoitive will be the preferred list making app. More to come!',
+        image: comingSoon,
+        alt: 'coming soon',
+        repoLink: 'https://www.google.com',
+        deployLink: 'https://www.google.com'
     },
     {
         picName: 'night',
         title: '',
         description: '',
         image: comingSoon,
-        alt: 'night sky',
-        repoLink: 'https://www.google.com',
-        deployLink: 'https://www.google.com'
-    },
-    {
-        picName: 'beach',
-        title: '',
-        description: '',
-        image: comingSoon,
-        alt: 'beach',
+        alt: 'coming soon',
         repoLink: 'https://www.google.com',
         deployLink: 'https://www.google.com'
     }
